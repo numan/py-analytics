f01b5cbb4da033bcd6c1761205d10de611290699
Numan S <numan@7geese.com>
2012-05-19 23:30:01 -0700

    Hooked up nose testing

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="analytics",
+    license='Apache License 2.0',
+    version="0.1.0",
+    description="Library for efficiently adding analytics to your project.",
+    long_description=open('README.rst', 'r').read(),
+    author="Numan Sachwani",
+    author_email="numan856@gmail.com",
+    url="https://github.com/numan/py-analytics",
+    packages=find_packages(exclude=['tests']),
+    test_suite='nose.collector',
+    install_requires=[
+        'nydus==0.9.0',
+        'redis==2.4.13',
+    ],
+    tests_require=[
+        'nose>=1.0',
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        'Intended Audience :: System Administrators',
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+)
