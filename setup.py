a6a614e7f133e0744f65d040c7b9b081b0baba13
Numan S <numan@7geese.com>
2012-09-11 13:00:33 -0700

    Minor version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="analytics",
     license='Apache License 2.0',
-    version="0.5.0",
+    version="0.5.1",
     description="Library for efficiently adding analytics to your project.",
     long_description=open('README.rst', 'r').read(),
     author="Numan Sachwani",
