feebb252472a9f9b2e2a08fc9937d27e3b337bec
Numan S <numan856@gmail.com>
2013-03-02 15:06:51 -0800

    Update requirements

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     test_suite='nose.collector',
     install_requires=[
-        'nydus==0.9.0',
-        'redis==2.4.13',
+        'nydus==0.10.4',
+        'redis==2.6.0',
         'python-dateutil==1.5',
     ],
     tests_require=[
